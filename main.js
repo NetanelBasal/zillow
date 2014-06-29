c61e3eca1a6b4e297f813bae34ef089e840eb2f7
netanel <phpnetanel@gmail.com>
2014-06-29 17:33:36 +0300

    phantom

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var http = require("http"),
-    cheerio = require("cheerio");
+    cheerio = require("cheerio"),
+    fs = require('fs'),
+    j = require('jquery');
 
 function download(url, callback) {
     http.get(url, function(res) {
@@ -15,38 +17,29 @@ function download(url, callback) {
     });
 
 }
+
 var assets = [];
-var url = "http://www.zillow.com/homes/for_sale/Bergen-County-NJ/874_rid/150000-200000_price/573-764_mp/days_sort/41.373202,-73.23967,40.48926,-75.023575_rect/9_zm/"
+var url = "http://www.zillow.com/homes/for_sale/Bergen-County-NJ/list/fsba,fsbo,new_lt/house,apartment_condo,duplex,townhouse_type/874_rid/150000-200000_price/574-765_mp/1_days/days_sort/41.29896,-73.611145,40.526848,-74.64798_rect/9_zm/1_p/0_mmm/";
 
 
 download(url, function(data) {
-
     if (data) {
+
         var $ = cheerio.load(data);
-        var numPages = $('ul.pagination-2012').find('li.next').prev().text();
-        for (var i = 2; i <= numPages; i++) {
-            var urlPaginate = url + [i] + '_p';
-            download(urlPaginate, function(data) {
-                var $ = cheerio.load(data);
-                $('dl.property-info-list > dt.type > strong').each(function(index) {
-                    if ($(this).text() == 'House For Sale') {
-                        var fullLink = $(this).parent().parent().next().first().find('a').attr('href');
-                        download('http://www.zillow.com' + fullLink, function(data) {
+        $('dt.property-address a.hdp-link').each(function(index) {
+            var price = $(this).parent().parent().parent().parent().next().find('dt.price-large').text();
+            var fullLink = $(this).attr('href');
+            download('http://www.zillow.com' + fullLink, function(data) {
+                var l = cheerio.load(data);
 
-                            // console.log(l('h1.prop-addr').text() + ' : ' + l('h2.forSale').children().last().text());
-                            var l = cheerio.load(data);
-                            assets.push({
-                                address: l('h1.prop-addr').text(),
-                                zentAstimate: l('.zest-value').eq(1).text()
-                            });
-                            console.log(assets);
-                        });
-                    }
-                })
+                var data =
+                    'address:' + l('h1.prop-addr').text() +
+                    ' ,price:' + price +
+                    ' ,zentAstimate:' + l('.zest-value').eq(1).text() + ' ';
 
+                fs.appendFile('data.txt', data)
             })
-        };
+        })
 
-
-    } else console.log("error");
+    }
 });
