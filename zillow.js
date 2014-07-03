0be9dd9cbd1b68ff23285b3db0e868a125c4f875
netanel <phpnetanel@gmail.com>
2014-07-03 22:09:09 +0300

    ready

--- zillow.js
+++ zillow.js
@@ -1,3 +1,12 @@
+// CONFIG
+
+var daysOnZillow = 7;
+//[0]bergan, [1]essex, [2]union, [3]hudson
+var places = ["http://www.zillow.com/homes/for_sale/Bergen-County-NJ/list/fsba,fsbo,new_lt/house,apartment_condo,duplex,townhouse_type/874_rid/150000-200000_price/571-761_mp/"+daysOnZillow+"_days/days_sort/41.133995,-73.893978,40.762114,-74.272483_rect/0_mmm/", "http://www.zillow.com/homes/for_sale/Essex-County-NJ/list/fsba,fsbo,new_lt/house,condo,apartment,duplex,townhouse_type/504_rid/150000-200000_price/570-761_mp/"+daysOnZillow+"_days/days_sort/40.90889,-74.112786,40.673903,-74.377516_rect/0_mmm/", "http://www.zillow.com/homes/for_sale/Union-County-NJ/list/fsba,fsbo,new_lt/house,condo,apartment,duplex,townhouse_type/771_rid/150000-200000_price/570-761_mp/"+daysOnZillow+"_days/days_sort/40.739229,-74.136702,40.591903,-74.46335_rect/0_mmm/", "http://www.zillow.com/homes/for_sale/Hudson-County-NJ/list/fsba,fsbo,new_lt/house,condo,apartment,duplex,townhouse_type/1106_rid/150000-200000_price/570-761_mp/"+daysOnZillow+"_days/days_sort/40.823569,-73.984882,40.642149,-74.166086_rect/0_mmm/"];
+
+// END CONFIG
+
+
 var http = require("http"),
     cheerio = require("cheerio"),
     fs = require('fs'),
@@ -20,13 +29,10 @@ function download(url, callback) {
     });
 
 }
-//[0]bergan, [1]essex, [2]union, [3]hudson
-var places = ["http://www.zillow.com/homes/for_sale/Bergen-County-NJ/list/fsba,fsbo,new_lt/house,apartment_condo,duplex,townhouse_type/874_rid/150000-200000_price/571-761_mp/1_days/days_sort/41.133995,-73.893978,40.762114,-74.272483_rect/0_mmm/", "http://www.zillow.com/homes/for_sale/Essex-County-NJ/list/fsba,fsbo,new_lt/house,condo,apartment,duplex,townhouse_type/504_rid/150000-200000_price/570-761_mp/1_days/days_sort/40.90889,-74.112786,40.673903,-74.377516_rect/0_mmm/", "http://www.zillow.com/homes/for_sale/Union-County-NJ/list/fsba,fsbo,new_lt/house,condo,apartment,duplex,townhouse_type/771_rid/150000-200000_price/570-761_mp/1_days/days_sort/40.739229,-74.136702,40.591903,-74.46335_rect/0_mmm/", "http://www.zillow.com/homes/for_sale/Hudson-County-NJ/list/fsba,fsbo,new_lt/house,condo,apartment,duplex,townhouse_type/1106_rid/150000-200000_price/570-761_mp/1_days/days_sort/40.823569,-73.984882,40.642149,-74.166086_rect/0_mmm/"];
 
 var assets = [];
 
 places.forEach(function(url, index) {
-
     download(url, function(data) {
         if (data) {
 
@@ -58,8 +64,13 @@ places.forEach(function(url, index) {
 
                     //get the link to tax table
                     var linkToTax = data.match(/asyncLoader.load\({"phaseType":"scroll","ajaxURL":"([^"]+)".*?home-tax-history/);
-
-                    if (linkToTax != null) {
+                    var objToPush = {
+                            "ratio": 'cant calc the ratio',
+                            "price": humanize.numberFormat(price) + '$',
+                            "address": address,
+                            "link": 'http://www.zillow.com' + fullLink,
+                            "area": url.substr(37, 16)
+                        };                    if (linkToTax != null) {
                         //there is tax table, get the json and crawel
                         download('http://www.zillow.com' + linkToTax[1], function(data) {
                             var d = JSON.parse(data);
@@ -69,26 +80,11 @@ places.forEach(function(url, index) {
 
 
                             var ratio = ((zentAstimate * 12) - parseInt(tax)) / (price);
-
-                            assets.push({
-                                "ratio": ratio.toFixed(2) + '%',
-                                "price": humanize.numberFormat(price) + '$',
-                                "address": address,
-                                "link": 'http://www.zillow.com' + fullLink,
-                                "area": url.substr(37, 16)
-                            });
+                            objToPush.ratio = ratio.toFixed(2) + '%';
 
                         })
-                    } else {
-                        //no tax table
-                        assets.push({
-                            "ratio": 'cant calc the ratio',
-                            "price": humanize.numberFormat(price) + '$',
-                            "address": address,
-                            "link": 'http://www.zillow.com' + fullLink,
-                            "area": url.substr(37, 16)
-                        });
                     }
+                    assets.push(objToPush);
                     json2csv({
                         data: assets,
                         fields: ['ratio', 'price', 'address', 'link', 'area']
